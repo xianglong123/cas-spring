f39c8fe03d692ceeea5abeeedf338d8112f3157b
xianglong123 <xianglong1233@163.com>
2022-12-28 14:13:56 +0800

    yh

---
+++ src/main/java/com/cas/config/ComponentTest.java
@@ -0,0 +1,13 @@
+package com.cas.config;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author xiang_long
+ * @version 1.0
+ * @date 2022/12/28 11:26 上午
+ * @desc
+ */
+@Component
+public class ComponentTest {
+}
---
+++ src/main/java/com/cas/config/ConfigTest.java
@@ -0,0 +1,13 @@
+package com.cas.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author xiang_long
+ * @version 1.0
+ * @date 2022/12/28 11:26 上午
+ * @desc
+ */
+@Configuration
+public class ConfigTest {
+}
